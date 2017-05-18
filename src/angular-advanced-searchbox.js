fb6534e03aa6bcf250291839e60f158c4a83b73c
Andreas Jacob <cordobo@web.de>
2017-05-18 17:01:27 +0200

    #4

--- src/angular-advanced-searchbox.js
+++ src/angular-advanced-searchbox.js
@@ -33,12 +33,13 @@ angular.module('angular-advanced-searchbox', [])
                     $scope.status = {
                         isopen: false
                     };
+                    $scope.model = $scope.model || [];
                     $scope.parameters = $scope.parameters || [];
                     $scope.filteredArr = $scope.parameters;
                     $scope.parametersLabel = $scope.parametersLabel || 'Parameter Suggestions';
                     $scope.parametersDisplayLimit = $scope.parametersDisplayLimit || 8;
                     $scope.placeholder = $scope.placeholder || 'Search ...';
-                    $scope.searchThrottleTime = $scope.searchThrottleTime || 750;
+                    $scope.searchThrottleTime = $scope.searchThrottleTime || 0;
                     $scope.searchParams = [];
                     $scope.searchQuery = '';
                     $scope.setFocusFor = setFocusFor;
@@ -61,9 +62,6 @@ angular.module('angular-advanced-searchbox', [])
 
                     $scope.$watch('model', function (newValue, oldValue) {
 
-                        console.log('newValue: ', newValue);
-                        console.log('oldValue: ', oldValue);
-
                         if (angular.equals(newValue, oldValue)) {
                             return;
                         }
@@ -74,7 +72,7 @@ angular.module('angular-advanced-searchbox', [])
                             console.log('$scope.$watch: ', a[iM]);
                         }
 
-                        /*
+                        /* @TODO START */
                         angular.forEach($scope.model, function (value, key) {
 
                             console.log('value: ', value);
@@ -129,7 +127,7 @@ angular.module('angular-advanced-searchbox', [])
                             }
                         });
 
-                        */
+                        /* @TODO END */
 
 
                         // delete not existing search parameters from internal state array
@@ -210,7 +208,7 @@ angular.module('angular-advanced-searchbox', [])
 
                     // QUERYMODE
                     $scope.searchQueryTypeaheadOnSelect = function (item, model, label) {
-                        console.log('Running $scope.searchQueryTypeaheadOnSelect: ', item, model, label);
+                        console.log('Running $scope.searchQueryTypeaheadOnSelect DELETE: ', item, model, label);
                         $scope.addSearchParam(item);
                         $scope.searchQuery = '';
                         updateModel('delete', 'query', 0);
@@ -218,6 +216,7 @@ angular.module('angular-advanced-searchbox', [])
 
                     // EDITMODE
                     $scope.searchParamTypeaheadOnSelect = function (suggestedValue, searchParam) {
+                        console.log('searchParamTypeaheadOnSelect - suggestedValue, searchParam: ', suggestedValue, searchParam);
                         searchParam.value = suggestedValue;
                         $scope.searchParamValueChanged(searchParam);
                     };
@@ -351,57 +350,6 @@ angular.module('angular-advanced-searchbox', [])
                         }
                     };
 
-
-                    // @TODO
-                    $scope.keyup = function(e, searchParamIndex) {
-                        console.log('DISPLAY $scope.keyup');
-
-                        var searchVal = $scope.searchQuery;
-                        var key = e.which || e.keyCode || e.charCode;
-
-                        console.log('DISPLAY searchVal: ', searchVal);
-                        console.log('DISPLAY $scope.searchQuery: ', $scope.searchQuery);
-
-                         // 8 backspace
-                         // 9 tab
-                         // 13 enter
-                         // 27 esc
-                         // 46 delete
-
-                        if (
-                            key !== 8 &&
-                            key !== 9 &&
-                            key !== 13 &&
-                            key !== 27 &&
-                            key !== 46
-                            ) {
-                            searchVal = searchVal + String.fromCharCode(key).toLowerCase();
-                            console.log('DISPLAY Mööp searchVal: ', searchVal);
-                        }
-
-                        if (searchVal == ' ') {  // space and field is empty, show menu
-                            $scope.showMenu(e);
-                            $timeout(function() {
-                                $scope.searchQuery = '';
-                            });
-                            return;
-                        }
-                        if (searchVal === '') {
-                            $scope.filteredArr = angular.copy($scope.parameters);
-                            // $scope.$apply();
-                            // $scope.$emit('textSearch', '', $scope.filter_keys);
-                            // if ($scope.facetSelected && $scope.facetSelected.options === undefined) {
-                            //     $scope.resetState();
-                            // }
-                            return;
-                        }
-                        if (key !== 8 || key !== 46) {
-                            $scope.filterFacets(e, searchVal);
-                        }
-
-                    };
-
-
                     $scope.keydown = function(e, searchParamIndex) {
 
                         var handledKeys = [8, 9, 13, 32, 37, 39];
@@ -443,6 +391,53 @@ angular.module('angular-advanced-searchbox', [])
                         }
                     };
 
+                    // @TODO
+                    $scope.keyup = function(e, searchParamIndex) {
+
+                        var searchVal = $scope.searchQuery;
+                        var key = e.which || e.keyCode || e.charCode;
+
+                         // 8 backspace
+                         // 9 tab
+                         // 13 enter
+                         // 27 esc
+                         // 46 delete
+
+                        if (
+                            key !== 8 &&
+                            key !== 9 &&
+                            key !== 13 &&
+                            key !== 27 &&
+                            key !== 46
+                            ) {
+                            // searchVal = searchVal + String.fromCharCode(key).toLowerCase();
+                            searchVal = searchVal.toLowerCase();
+                        }
+
+                        if (searchVal == ' ') {  // space and field is empty, show menu
+                            $scope.showMenu(e);
+                            $timeout(function() {
+                                $scope.searchQuery = '';
+                            });
+                            return;
+                        }
+                        if (searchVal === '') {
+                            $scope.filteredArr = angular.copy($scope.parameters);
+                            // $scope.$apply();
+                            // $scope.$emit('textSearch', '', $scope.filter_keys);
+                            // if ($scope.facetSelected && $scope.facetSelected.options === undefined) {
+                            //     $scope.resetState();
+                            // }
+                            return;
+                        }
+                        if (key !== 8 || key !== 46) {
+                            $scope.filterFacets(e, searchVal);
+                        }
+
+                    };
+
+
+
                     $scope.isMatchLabel = function(label) {
                         return Array.isArray(label);
                     };
@@ -456,7 +451,6 @@ angular.module('angular-advanced-searchbox', [])
                             idx = facet.name.toLowerCase().indexOf(searchVal);
                             if (idx > -1) {
                                 label = [facet.name.substring(0, idx), facet.name.substring(idx, idx + searchVal.length), facet.name.substring(idx + searchVal.length)];
-                                // var pushObj = {'name':facet.name, 'label':label, 'options':facet.options};
                                 var pushObj = facet;
                                 pushObj.label = label;
                                 filtered.push(pushObj);
@@ -529,45 +523,42 @@ angular.module('angular-advanced-searchbox', [])
                             value: value
                         });
 
+
                         searchThrottleTimer = $timeout(function () {
-                            console.log('searchThrottleTimer: ', searchThrottleTimer);
-                            console.log('changeBuffer: ', JSON.stringify(changeBuffer));
 
                             angular.forEach(changeBuffer, function (change) {
-                                console.log('changeBuffer change: ', change);
+
                                 var searchParam = $filter('filter')($scope.parameters, function (param) { return param.key === key; })[0];
 
-                                console.log('searchThrottleTimer searchParam: ', searchParam);
+                                console.log('DISPLAY changeBuffer change: ', change);
+                                // console.log('changeBuffer searchParam: ', searchParam);
 
-                                if (searchParam && searchParam.allowMultiple) {
-
-                                    console.log('Running searchParam.allowMultiple change: ', change);
-                                    // $scope.model.push(change);
-                                    delete change.command;
-                                    $scope.model.splice(change.index, 1, change);
-
-                                    /*
-                                    if (!angular.isArray($scope.model[change.key])) {
-                                        $scope.model[change.key] = [];
-                                    }
-
-                                    if (change.command === 'delete') {
-                                        $scope.model[change.key].splice(change.index, 1);
-                                        if ($scope.model[change.key].length === 0) {
-                                            delete $scope.model[change.key];
-                                        }
-                                    } else {
-                                        $scope.model[change.key][change.index] = change.value;
-                                    }
-                                    */
-                                } else {
-                                    if (change.command === 'delete') {
-                                        delete $scope.model[change.key];
-                                    }
-                                    else {
-                                        $scope.model[change.key] = change.value;
+                                if (change.key === 'query' && change.command === 'delete') {
+                                    $scope.searchQuery = '';
+                                    return;
+                                }
+                                else if (change.command === 'delete') {
+                                    var index = $scope.model.map(function(o) {
+                                        return o.index;
+                                    }).indexOf(change.index);
+                                    console.log('changeBuffer change.index: ', change.index);
+                                    console.log('changeBuffer index: ', index);
+                                    if (index > -1) {
+                                        $scope.model.splice(index, 1);
                                     }
                                 }
+                                else if (change.value === undefined || change.value === '') {
+                                    // $timeout.cancel(searchThrottleTimer);
+                                    return;
+                                }
+                                else {
+                                    delete change.command;
+                                    $scope.model.splice(change.index, 1, change);
+                                }
+
+                                // @TODO
+                                // if (searchParam && searchParam.allowMultiple) {
+                                // }
                             });
 
                             changeBuffer.length = 0;
